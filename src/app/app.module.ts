67af153bdd14584299c599d36c2702165fbf66e9
SouvickMondal <souvickmondalp7@gmail.com>
2018-07-07 16:53:27 +0530

    Login  form Template to Reactive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { HeroComponent } from './hero/hero.component';
 import { AuthModule } from './auth/auth.module';
 import { CoreModule } from './core/core.module';
-<<<<<<< HEAD
 import { AdminModule } from './admin/admin.module';
-=======
 import { StudentModule } from './student/student.module';
 import { MentorModule } from './mentor/mentor.module';
->>>>>>> 96203390b38662dbc38bc45171642b6ab4c6ceb5
 
 
 @NgModule({
@@ -20,11 +17,7 @@ import { MentorModule } from './mentor/mentor.module';
     HeroComponent
   ],
   imports: [
-<<<<<<< HEAD
-    BrowserModule,CoreModule,AuthModule,AppRoutingModule,AdminModule
-=======
-    BrowserModule,CoreModule,AuthModule,StudentModule,MentorModule,AppRoutingModule
->>>>>>> 96203390b38662dbc38bc45171642b6ab4c6ceb5
+    BrowserModule,CoreModule,AuthModule,StudentModule,MentorModule,AppRoutingModule,AdminModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/auth/login.component.ts
+++ src/app/auth/login.component.ts
@@ -1,17 +1,52 @@
 import { Component, OnInit } from '@angular/core';
 import { CoreService } from '../core/core.service';
 import { Router } from '@angular/router';
+import { FormGroup,FormBuilder, Validators, AbstractControl } from "@angular/forms";
+import { Observable } from "rxjs";
+// import  "rxjs/add/operator/debounceTime";
 
 @Component({
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  userMessage : string;
+  passwordMessage : string;
 
-  constructor(private _route:Router,private _coreService:CoreService) { }
+  constructor(private _route:Router,private _coreService:CoreService, private fb:FormBuilder) { }
+
 
   ngOnInit() {
+    this.loginForm = this.fb.group({
+      UserName:['',Validators.required],
+      Password:['',Validators.required]
+    });
+    this.loginForm.get("UserName").valueChanges.subscribe(value=>
+      this.userNotify(this.loginForm.get("UserName")));
+    this.loginForm.get("Password").valueChanges.subscribe(value=>
+      this.passwordNotify(this.loginForm.get("Password")));
   }
+  private validationMessages = {
+    required:"Please fill this field"
+    
+  }
+  userNotify(c : AbstractControl):void {
+    this.userMessage = '';//Clear the previous messages if any
+    if((c.touched || c.dirty)&& c.errors){
+      this.userMessage = Object.keys(c.errors).map(key=>
+        this.validationMessages[key]).join('');
+    }
+  }
+
+  passwordNotify(c : AbstractControl):void {
+    this.passwordMessage = '';//Clear the previous messages if any
+    if((c.touched || c.dirty)&& c.errors){
+      this.passwordMessage = Object.keys(c.errors).map(key=>
+        this.validationMessages[key]).join('');
+    }
+  }
+  
 
 
   log(){
@@ -27,5 +62,45 @@ export class LoginComponent implements OnInit {
         //waiting
       }
     }
-  } 
+  }
+  // console.log(this.loginForm); 
 }
+
+
+
+
+
+
+// previous Code
+
+// import { Component, OnInit } from '@angular/core';
+// import { CoreService } from '../core/core.service';
+// import { Router } from '@angular/router';
+
+// @Component({
+//   templateUrl: './login.component.html',
+//   styleUrls: ['./login.component.css']
+// })
+// export class LoginComponent implements OnInit {
+
+//   constructor(private _route:Router,private _coreService:CoreService) { }
+
+//   ngOnInit() {
+//   }
+
+
+//   log(){
+//     this._coreService.authenticate();
+//     if(this._coreService.isAuthenticated){
+//       if(this._coreService.isStudent){
+//         this._route.navigate(["/student/username"]);
+//       }
+//       else if(this._coreService.isMentor){
+//         this._route.navigate(["/mentor/username"]);
+//       }
+//       else if(this._coreService.isAdmin){
+//         //waiting
+//       }
+//     }
+//   } 
+// }
